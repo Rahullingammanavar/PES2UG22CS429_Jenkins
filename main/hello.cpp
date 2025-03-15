84edc16c32d863d46d0fe1d82356c420086046fe
Rahullingammanavar <rahullingammanavar@gmail.com>
2025-03-15 23:26:28 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout<<"Hello PES\n"
+  cout<<"Hello PES H\n"
 ;  cout << "I have successfully built and run this  \n";
   return 0;
 }
